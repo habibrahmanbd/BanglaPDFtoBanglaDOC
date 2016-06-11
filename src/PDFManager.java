5c53e366c8811ce2ca137b167900b251b8f84369
Habib Rahman <bdhabib94@gmail.com>
2016-06-12 02:46:05 +0600

    Update PDFManager.java

--- src/PDFManager.java
+++ src/PDFManager.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package banglapdftobangladoc;
+package bp2bd;
 
 import java.io.File;
 import java.io.IOException;
